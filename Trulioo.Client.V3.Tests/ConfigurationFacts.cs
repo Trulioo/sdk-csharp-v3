7730532444aa46158df55ae42a353e6ee2a10b49
trulioodevops <57775951+trulioodevops@users.noreply.github.com>
2024-06-20 10:00:08 -0700

    "KYB-4861 configuration endpoints" (#17)
    [
      {
        "No": 2,
        "original_sha": "63d71370a3c85c9e26a7a672061f66eaeff3b269",
        "title": "unit tests",
        "message": "unit tests\n"
      },
      {
        "No": 1,
        "original_sha": "4048f15f810482e728aa64f507b006f0091c035b",
        "title": "KYB-4861 configuration endpoints",
        "message": "KYB-4861 configuration endpoints\n"
      }
    ]
    
    Co-authored-by: jason.attai.trulioo <jason.attai@trulioo.com>

--- Trulioo.Client.V3.Tests/ConfigurationFacts.cs
+++ Trulioo.Client.V3.Tests/ConfigurationFacts.cs
@@ -10,9 +10,9 @@ namespace Trulioo.Client.V3.Tests
         [Fact(Skip = "Calls API")]
         public async Task GetAllDatasourcesTest()
         {
-            using (var client = await BaseFact.GetTruliooClientAsync())
+            using var client = await BaseFact.GetTruliooClientAsync();
             {
-                var response = await client.Configuration.GetAllDatasourcesAsync("packageid");
+                var response = await client.Configuration.GetAllDatasourcesAsync(BaseFact.PackageId);
                 var datasources = new List<string>();
 
                 foreach (var r in response)
@@ -22,6 +22,100 @@ namespace Trulioo.Client.V3.Tests
 
                 Assert.NotEmpty(response);
                 Assert.NotEmpty(datasources);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetConsentsTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetСonsentsAsync(BaseFact.PackageId, BaseFact.CountryCode);
+
+                Assert.NotNull(response);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetDetailedConsentsTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetDetailedСonsentsAsync(BaseFact.PackageId, BaseFact.CountryCode);
+
+                Assert.NotNull(response);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetCountryCodesTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetCountryCodesAsync(BaseFact.PackageId);
+
+                Assert.NotNull(response);
+                Assert.NotEmpty(response);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetCountrySubdivisionsTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetCountrySubdivisionsAsync(BaseFact.CountryCode);
+
+                Assert.NotNull(response);
+                Assert.NotEmpty(response);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetFieldsTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetFieldsAsync(BaseFact.PackageId, BaseFact.CountryCode);
+
+                Assert.NotNull(response);
+                Assert.NotEmpty(response);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetRecommendedFieldsTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetRecommendedFieldsAsync(BaseFact.PackageId, BaseFact.CountryCode);
+
+                Assert.NotNull(response);
+                Assert.NotEmpty(response);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetTestEntitiesTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetTestEntitiesAsync(BaseFact.PackageId, BaseFact.CountryCode);
+
+                Assert.NotNull(response);
+                Assert.NotEmpty(response);
+            }
+        }
+
+        [Fact(Skip = "Calls API")]
+        public async Task GetDatasourcesTest()
+        {
+            using var client = await BaseFact.GetTruliooClientAsync();
+            {
+                var response = await client.Configuration.GetDatasourcesAsync(BaseFact.PackageId, BaseFact.CountryCode);
+
+                Assert.NotNull(response);
+                Assert.NotEmpty(response);
             }
         }
     }
---
+++ Trulioo.Client.V3/Models/Fields/TestEntityDataFields.cs
@@ -0,0 +1,13 @@
+﻿namespace Trulioo.Client.V3.Models.Fields
+{
+    /// <summary>
+    /// The data field name-value pairs for the data elements on which the verification is to be performed
+    /// </summary>
+    public class TestEntityDataFields : DataFields
+    {
+        /// <summary>
+        /// Friendly name for the test entity
+        /// </summary>
+        public string TestEntityName { get; set; }
+    }
+}
